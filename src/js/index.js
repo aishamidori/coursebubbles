827f2b84d307fae087e39fe19bb8268ef7710df7
Aisha F <aisha@cs.brown.edu>
2013-12-07 15:21:48 -0500

    added Search spots for searching and made results clickable to add

--- src/js/index.js
+++ src/js/index.js
@@ -7,45 +7,50 @@ function CourseBubbles() {
   self.semesters = ko.observableArray([
       {name:"Fall '13", 
         courses: ko.observableArray([
-                   {name: "Law"},
-                   {name: "Systems"},
-                   {name: "Language"},
-                   {name: "Computer"},
+                   {name: "Search"},
                    ])
       },
       {name:"Spring '14", 
         courses: ko.observableArray([
-                   {name: "Economics"},
-                   {name: "Gender"},
-                   {name: "English"},
+                   {name: "Search"},
                    ])
       }
   ]);
   self.results = ko.observableArray([]);
   self.searchTerm = ko.observable("");
-
   self.cart = ko.observableArray([
-              {name: "Economics"},
-              {name: "Gender"},
-              {name: "English"},
-            ]);
+      {name: "Search"},
+  ]);
 
 
+  self.searchSemester = "";
+  self.addCourse = function(result){
+    semester = _.find(courseBubbles.semesters(), function(sem) {
+      console.log(sem);
+      return (sem.name == self.searchSemester);
+    });
+    search = semester.courses.pop(); 
+    semester.courses.push(result);
+    semester.courses.push(search);
+    addListeners();
+  };
 }
 
 function addListeners() {
   $('.course').mousedown(function(e) {
-    var rect = e.target.getBoundingClientRect();
-    $("#course-add-er").removeClass("hidden-add-er");
-    $("#course-add-er").css("top", rect.top - 150);
-    $("#course-add-er").css("left", rect.right);
+    console.log(e);
+    if ($(e.currentTarget).hasClass("search")) {
+      semester = $(e.currentTarget).parents('.semester');
+      courseBubbles.searchSemester = $($(semester[0]).children('h2')[0]).text();
+      console.log("searching with semester " + courseBubbles.searchSemester);
+      var rect = e.target.getBoundingClientRect();
+      $("#course-add-er").removeClass("hidden-add-er");
+      $("#course-add-er").css("top", rect.top - 150);
+      $("#course-add-er").css("left", rect.right);
+    }
   });
-
-  /*$('.course').mouseup(function(e) {
-    console.log(e.target);
-    //$(e.target).css("position", "relative");
-  });*/
 }
+
 
 $(document).ready(function() {
   courseBubbles = new CourseBubbles();
@@ -58,5 +63,12 @@ $(document).ready(function() {
     courseBubbles.results(results.slice(0, 3));
     console.log(courseBubbles.results());
     console.log("done querying");
+    //addCourse(courseBubbles.searchSemester, results[0]);
   });
 });
+
+function toggleCart(){
+  var contentID = document.getElementById("cart");
+  contentID.style.display == "block" ? contentID.style.display = "none" : 
+      contentID.style.display = "block";
+}
